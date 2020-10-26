d260aa27cfcc531bdbaf6df9e46b5de39d09163b
Raphael Assis <58819530+AssisRaphael@users.noreply.github.com>
2020-10-26 16:17:41 -0300

    Update greeting.sh

--- greeting.sh
+++ greeting.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 :'
-Escrever um shell script para cumprimentar um usuário com "Bom dia", "Boa tarde" ou "Boa noite" dependendo do horário do sistema. A saudação deve incluir o nome completo do usuário atualmente logado. Dica: use o comando whoami para obter o usuário atual e depois consulte o arquivo /etc/passwd na quinta coluna para obter o seu nome completo. As horas do dia são divididas da seguinte maneira:
+Escrever um shell script para cumprimentar um usuário com "Bom dia", "Boa tarde" ou
+"Boa noite" dependendo do horário do sistema. A saudação deve incluir o nome completo
+do usuário atualmente logado. Dica: use o comando whoami para obter o usuário atual e
+depois consulte o arquivo /etc/passwd na quinta coluna para obter o seu nome completo.
+As horas do dia são divididas da seguinte maneira:
 
 • Dia: até 12:00
 • Tarde: a partir de 12:00 e até 18:00
