95bc0d9a19ebbe64e9ee9c6f060f770e07896480
Raphael Assis <58819530+AssisRaphael@users.noreply.github.com>
2020-10-26 16:16:10 -0300

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 :'
-O comando expr é capaz de realizar cálculos com expressões aritméticas inteiras. Reescreva um programa similar em shell script para calcular uma expressão com apenas um operador (+, -, x, /) com dois números em ponto flutuante e imprima o resultado em ponto flutuante. Você deve usar a estrutura case como controle de fluxo condicional. O programa deve ler os dois números e o operador via parâmetros em linha de comando. Caso não sejam especificados os três parâmetros, deve-se mostrar uma mensagem de ajuda. Se o operador não for um dos quatro suportados, deve-se mostrar uma mensagem de erro. Dica: use o comando bc (basic calculator) para fazer as operações em ponto-flutuante.
+O comando expr é capaz de realizar cálculos com expressões aritméticas inteiras. 
+Reescreva um programa similar em shell script para calcular uma expressão com apenas 
+um operador (+, -, x, /) com dois números em ponto flutuante e imprima o resultado em 
+ponto flutuante. Você deve usar a estrutura case como controle de fluxo condicional. 
+O programa deve ler os dois números e o operador via parâmetros em linha de comando. 
+Caso não sejam especificados os três parâmetros, deve-se mostrar uma mensagem de ajuda. 
+Se o operador não for um dos quatro suportados, deve-se mostrar uma mensagem de erro. 
+Dica: use o comando bc (basic calculator) para fazer as operações em ponto-flutuante.
 
 Exemplo de uso do shell script:
 
